36ac6c44a20c1341e31b50e397e27a480572439e
Danish <danishmaniyar2110@gmail.com>
2024-04-22 19:35:00 +0530

    Completed some part of Mapping Relations

--- src/main/java/com/danish/HibernateLearning2/App.java
+++ src/main/java/com/danish/HibernateLearning2/App.java
@@ -1,13 +1,35 @@
 package com.danish.HibernateLearning2;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    @SuppressWarnings("deprecation")
+	public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	Laptop laptop=new Laptop();
+    	laptop.setLid(101);
+    	laptop.setLname("Dell");
+    	
+    	Student s=new Student();
+    	s.setName("Danish");
+    	s.setRollno(1);
+    	s.setMarks(50);
+    	
+    	Configuration con=new Configuration().configure().addAnnotatedClass(Laptop.class).addAnnotatedClass(Student.class);
+    	SessionFactory sf=con.buildSessionFactory();
+        Session session=sf.openSession();
+        Transaction tx=session.beginTransaction();
+        
+        session.save(laptop);
+        session.save(s);
+        tx.commit();
     }
 }
---
+++ src/main/java/com/danish/HibernateLearning2/Student.java
@@ -0,0 +1,31 @@
+package com.danish.HibernateLearning2;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Student {
+	@Id
+     private int rollno;
+     private String name;
+     private int marks;
+	public int getRollno() {
+		return rollno;
+	}
+	public void setRollno(int rollno) {
+		this.rollno = rollno;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getMarks() {
+		return marks;
+	}
+	public void setMarks(int marks) {
+		this.marks = marks;
+	}
+     
+}
